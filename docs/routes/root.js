b7a514d4da292314ed0867eb36f8e9c27f8aecb7
Jacob Ebey <jacob.ebey@live.com>
2023-01-10 01:26:37 -0800

    inline css

--- docs/routes/root.js
+++ docs/routes/root.js
@@ -1,5 +1,7 @@
 import { html } from "html-tagged";
 
+import mvpcss from "./mvp.css.js";
+
 export default function Root() {
   return html`
     <!DOCTYPE html>
@@ -7,8 +9,8 @@ export default function Root() {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="stylesheet" href="https://unpkg.com/mvp.css@1.12/mvp.css" />
         <title>html-tagged</title>
+        ${mvpcss}
       </head>
       <body>
         <header>
