0341a23b87722c65a165c8cde534428ab4fedcdb
Jacob Ebey <jacob.ebey@live.com>
2023-07-26 00:19:55 -0700

    update layout

--- docs/routes/layout.ts
+++ docs/routes/layout.ts
@@ -6,7 +6,8 @@ export default function DocumentLayout() {
     <html lang="en">
       <head>
         <meta charset="utf-8" />
-        <title>My App</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>HTML Tagged</title>
         <link
           rel="stylesheet"
           href="https://unpkg.com/prismjs@1.29.0/themes/prism-okaidia.min.css"
